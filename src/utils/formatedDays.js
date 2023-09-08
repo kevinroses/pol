071b9785823da1cc6773801b6e2829dac3b33fdb
kentur <kevinxrturop@gmail.com>
2023-09-08 11:12:06 -0500

    Update formatedDays.js

--- src/utils/formatedDays.js
+++ src/utils/formatedDays.js
@@ -1,11 +1,13 @@
 import moment from "moment";
 
-export const currentDate = moment().format("YYYY/MM/DD HH:mm");
-export const nextday = moment(currentDate).add(1, "days").format("YYYY/MM/DD");
+// Obtén la fecha y hora actual en el formato ISO 8601
+export const currentDate = moment().format("YYYY-MM-DDTHH:mm:ssZ");
 
+// Calcula la fecha del día siguiente
+export const nextday = moment(currentDate).add(1, "days").format("YYYY-MM-DD");
+
+// Obtén el nombre del día de la semana actual
 export const today = moment(currentDate).format("dddd");
+
+// Obtén el nombre del día de la semana para el día siguiente
 export const tomorrow = moment(nextday).format("dddd");
-
-export const CurrentDatee = moment().format();
-
-export const todayTime = moment(CurrentDatee).format("HH:mm");
